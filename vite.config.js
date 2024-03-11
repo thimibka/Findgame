1299fdaaa2ffd8505b782b404b2cf86f7092b1ad
gervais <fseetp@gmail>
2024-03-11 22:08:40 +0100

    key

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.API_KEY,
+  base: process.env.KEY,
   plugins: [react()],
 });
